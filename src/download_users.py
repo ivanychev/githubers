bd213084dad4c2d0f064b5aa749bd222441a4440
Sergey Ivanchev <sergeyivanychev@gmail.com>
2016-11-29 22:14:08 +0300

    Implemented saving users to SQLite  db instead of files

--- src/download_users.py
+++ src/download_users.py
@@ -2,9 +2,10 @@
 
 import sys
 import config
-import githubers
-
+import githubers as g
 CONFIG_PATH = "../resources/config.json"
+
+
 def check_args(argv):
     if len(argv) != 3:
         print("Github login and password are expected as script parameters")
@@ -12,18 +13,18 @@ def check_args(argv):
     return True
 
 
-
 def main(argv):
     if not check_args(argv):
         return -1
     login, password = argv[1], argv[2]
-    path = config.read_config(CONFIG_PATH)
-    last_id = githubers.get_biggest_id(path)
-    while last_id is not None:
-        users_jsons = githubers.get_next_users(last_id, 1000, login, password)
-        last_id = githubers.save_users(users_jsons, path)
-        print("Last id in base: " + str(last_id))
-
+    db_path = config.read_config(CONFIG_PATH)
+    conn = g.connect_db(db_path)
+    cursor = conn.cursor()
+    max_id = g.max_user_id(conn)
+    for user_tuple in g.remote_users(login, password, since_id=max_id):
+        g.add_user(cursor, user_tuple)
+        conn.commit()
+        print("added {}:{}".format(user_tuple.UserID, user_tuple.Login))
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
--- src/githubers.py
+++ src/githubers.py
@@ -1,27 +1,84 @@
-
-from os import listdir
+from os.path import isfile
 import requests
-import json
+from toolz import *
+from toolz.curried import *
+import sqlite3
+from collections import namedtuple
 
-def get_biggest_id(path):
-    files = listdir(path)
-    string_ids = (f.rstrip(".json") for f in files if f.endswith(".json"))
-    ids = map(lambda x: int(x), string_ids)
-    if len(files) == 0:
-        return 0
-    return max(ids)
+USER_FIELDS = ("UserID", "Login", "Type", "Created_at", "OrganisationsURL", "FollowingURL", "UserURL")
+USER_FIELDS_JSON = ("id", "login", "type", "organizations_url", 'subscriptions_url', 'url')
+USER_FACTORY = namedtuple("User_repr", USER_FIELDS)
+USERS_INIT_COMMAND = """create table Users
+(
+   {} integer primary key,
+   {} text not null,
+   {} text not null,
+   {} text,
+   {} text,
+   {} text,
+   {} text
+)
+""".format(*USER_FIELDS)
+FOLLOWINGS_FIELDS = ("Follower", "Following")
+FOLLOWINGS_INIT_COMMAND = """create table Followers
+(
+  {} integer not null,
+  {} integer not null
+)
+""".format(*FOLLOWINGS_FIELDS)
+MEMBERSHIP_FIELDS = ("OrganisationID", "UserID", "PublicityFlag")
+MEMBERSHIP_INIT_COMMAND = """create table Membership
+(
+  {} integer not null,
+  {} integer not null,
+  {} integer
+)
+""".format(*MEMBERSHIP_FIELDS)
 
-def get_next_users(since_id, number, login, password):
-    req = "https://api.github.com/users?since=" + str(since_id) + "&per_page=" + str(number)
-    r = requests.get(req, auth=(login, password))
-    return r.json()
 
-def save_users(jsons, path):
-    if len(jsons) == 0:
-        return None
-    for x in jsons:
-        f = open(path + str(x["id"]) + ".json", "w+")
-        f.write(json.dumps(x, sort_keys=True, indent=2))
-        f.close()
-        last_id = x["id"]
-    return last_id
+def compose_named_tuple(raw_tuple):
+    raw = list(raw_tuple)
+    raw.insert(3, None)
+    return USER_FACTORY(*raw)
+
+def remote_users(login, password, since_id=0, per_request=100):
+    print("Hello")
+    end = False
+    if since_id is None:
+        since_id = 0
+    process = compose(compose_named_tuple, get(list(USER_FIELDS_JSON)))
+    while not end:
+        req = "".join(["https://api.github.com/users?since=", str(since_id), "&per_page=", str(per_request)])
+        response = requests.get(req, auth=(login, password))
+        end = len(req) == 0
+        if end:
+            break
+        jsons = response.json()
+        since_id = max(map(get("id"), jsons))
+        yield from map(process, jsons)
+
+
+def configure_db(conn):
+    c = conn.cursor()
+    c.execute(USERS_INIT_COMMAND)
+    c.execute(FOLLOWINGS_INIT_COMMAND)
+    c.execute(MEMBERSHIP_INIT_COMMAND)
+
+
+def connect_db(path):
+    is_db = isfile(path)
+    conn = sqlite3.connect(path)
+    if not is_db:
+        configure_db(conn)
+    return conn
+
+
+def max_user_id(conn):
+    c = conn.cursor()
+    c.execute("""select max({})
+    from Users""".format(USER_FIELDS[0]))
+    return list(c)[0][0]
+
+def add_user(cursor, user_tuple):
+    cursor.execute("insert into Users values (?,?,?,?,?,?,?)", user_tuple)
+
